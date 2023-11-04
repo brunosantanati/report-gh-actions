a3c99305481db6a5fda76e8bfc6a7bb24ca9b029
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-11-04 18:33:48 -0300

    Upload artifact using a lib

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const core = require('@actions/core')
 //const github = require('@actions/github')
-const exec = require('@actions/exec')
+//const exec = require('@actions/exec')
 const https = require('https')
 const { writeToPath } = require('@fast-csv/format')
+const artifact = require('@actions/artifact');
 
 function run() {
     const apiUrl = core.getInput('apiUrl', { required: true })
@@ -35,10 +36,25 @@ function generateCsvReport(products) {
         .on('error', err => console.error(err))
         .on('finish', () => {
             console.log('CSV Report is ready!')
-            const csvContent = await exec.exec(`cat ${path}`)
-            console.log(`Report content: ${csvContent}`)
-            core.setOutput('csvContent', csvContent)
+            uploadArtifact()
         })
+}
+
+function uploadArtifact() {
+    const artifactClient = artifact.create()
+    const artifactName = 'report-artifact';
+    const files = [
+        'products.csv'
+    ]
+    
+    const rootDirectory = '.' // Also possible to use __dirname
+    const options = {
+        continueOnError: false
+    }
+    
+    const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+
+    console.log(uploadResponse)
 }
 
 run()
