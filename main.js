e67d4a8ff380e711b6ffd165e658c6c2a38d6f98
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-11-04 17:13:08 -0300

    Generate the report and set its content as output

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+//const github = require('@actions/github')
 const exec = require('@actions/exec')
+const https = require('https')
+const { writeToPath } = require('@fast-csv/format')
+const { exec } = require('child_process')
 
 function run() {
     const apiUrl = core.getInput('apiUrl', { required: true })
 
-    console.log(`apiUrl = ${apiUrl}`)
+    https.get(apiUrl + '/products', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            let products = JSON.parse(data)
+            generateCsvReport(products)
+        });
+
+        }).on("error", (err) => {
+        console.log("Error: " + err.message)
+    })
+}
+
+function generateCsvReport(products) {
+    const path = 'products.csv';
+    data = products
+    const options = { headers: true, quoteColumns: true }
+
+    writeToPath(path, data, options)
+        .on('error', err => console.error(err))
+        .on('finish', () => console.log('CSV Report is ready!'))
+
+    const csvContent = exec.exec(`cat ${path} | base64`)
+    core.setOutput('csvContent', csvContent)
 }
 
 run()
